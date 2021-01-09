230f14fd97f2bcbe7aab9f3d963a4f7d248b0cff
Andrei Alvarez <andreialvarezf@gmail.com>
2021-01-09 16:06:15 -0500

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,28 +4,38 @@ import headerImage from "../images/Ironhack Post.png";
 import moment from "moment";
 
 export default function Header() {
-  const [search, setSearch] = useState("")
+  const [search, setSearch] = useState("");
   let dateCreate = moment().format("ddd, MMMM DD, YYYY");
 
   const handleChange = (event) => {
     setSearch(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
-    window.location.href=`/search/${search}`
+    window.location.href = `/search/${search}`;
     event.preventDefault();
-  }
+  };
 
   return (
-    <div className="header">
-      <p className="date">{dateCreate}</p>
-      <Link to="/" className="logo">
+    <section className="header">
+    <div className="header-one">
+       <Link to="/" className="logo">
         <img id="header__image" src={headerImage} alt="img"></img>
       </Link>
+    </div>
+
+
+      <p className="date">{dateCreate}</p>
+     
       <form className="search-form" onSubmit={handleSubmit}>
-        <input className="search-input" placeholder="   Search:" name="search" onChange={handleChange} />
+        <input
+          className="search-input"
+          placeholder="   Search:"
+          name="search"
+          onChange={handleChange}
+        />
         {/* <button className="search-button">Go</button> */}
       </form>
-    </div>
+    </section>
   );
 }
