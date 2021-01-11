72163d9afa44d59198d0db0cad74287447ffc95c
Andrei Alvarez <andreialvarezf@gmail.com>
2021-01-11 17:54:09 -0500

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
 import footerImage from "../images/News-footer.jpg";
 
 export default function Footer() {
-    return (
-        <div className="footer">
-            <img id="footer__image" src={footerImage} alt="img"></img> 
-        </div>
-    )
+  return (
+    <div className="footer">
+      <img id="footer__image" src={footerImage} alt="img"></img>
+      <h5>Copyright © 2021 · Andrei Alvarez</h5>
+    </div>
+  );
 }
